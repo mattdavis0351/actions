4de1837ad34312ddc61efc9e90d0cc935bcb6c41
mattdavis0351 <mattdavis0351@github.com>
2019-09-20 13:53:11 -0700

    adding inital code

---
+++ open-pull-request/src/main.ts
@@ -0,0 +1,28 @@
+import * as github from '@actions/github';
+import * as core from '@actions/core';
+
+async function run() {
+	const pullTitle = core.getInput('pullTitle');
+	const myApiToken = core.getInput('apiToken');
+	const baseBranch = core.getInput('baseBranch');
+	const headBranch = core.getInput('headBranch');
+	const octokit = new github.GitHub(myApiToken);
+	const context = github.context;
+
+	try {
+		// const newPull = await octokit.pulls.create({
+		// 	owner: context.repo.owner,
+		// 	repo: context.repo.repo,
+		// 	title: pullTitle,
+		// 	base: baseBranch,
+		// 	head: headBranch
+		// });
+
+		// core.debug(JSON.stringify(newPull));
+		core.debug(JSON.stringify(context));
+	} catch (error) {
+		core.debug(error.message);
+	}
+}
+
+run();
