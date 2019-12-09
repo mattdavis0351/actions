ed56b9b936512c4e1519e06142f13f501f29cc91
mattdavis0351 <mattdavis0351@github.com>
2019-12-09 11:25:06 -0800

    refactor issue-maker

---
+++ issue-maker/main.js
@@ -0,0 +1,37 @@
+const core = require("@actions/core");
+const github = requrie("@actions/github");
+const fm = require("front-matter");
+const fs = require("fs");
+
+function craftArray(list) {
+  if (!list) return [];
+  return Array.isArray(list) ? list : list.split(", ");
+}
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token");
+    const file = fs.readFileSync(`./${core.getInput("filename")}`);
+    const assignees = core.getInput("assigness");
+    // const issueTitle = core.getInput("issueTitle");
+    // const issueBody = core.getInput("issueBody");
+
+    const octokit = new github.GitHub(token);
+    const context = github.context;
+
+    const { attributes, body } = fm(file);
+
+    const newIssue = await octokit.issues.create({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      title: attributes.title,
+      body: body,
+      assignees: assignees ? craftArray(assignees) : craftArray(attributes.assignees),
+      labels: craftArray(attributes.labels)
+    });
+  } catch (error) {
+    core.debug(error.message);
+  }
+}
+
+run();
