c2557fe74986d9fc33d3146103d040fa6378472c
mattdavis0351 <mattdavis0351@github.com>
2019-12-09 15:44:29 -0800

    fixed spelling

--- issue-maker/main.js
+++ issue-maker/main.js
@@ -2,44 +2,45 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fm = require("front-matter");
 const fs = require("fs");
-const nunjucks = require('nunjucks');
-const dateFilter = require('nunjucks-date-filter');
-
+const nunjucks = require("nunjucks");
+const dateFilter = require("nunjucks-date-filter");
 
 function craftArray(item) {
   if (!item) return [];
-  return Array.isArray(item) ? item : item.split(', ');
+  return Array.isArray(item) ? item : item.split(", ");
 }
 
 async function run() {
   try {
     const token = core.getInput("repo-token");
     const file = fs.readFileSync(`./${core.getInput("filename")}`);
-    const assignees = core.getInput("assigness");
-    const env = nunjucks.configure({autoescape: false})
-    env.addFilter('date', dateFilter)
+    const assignees = core.getInput("assignees");
+    const env = nunjucks.configure({ autoescape: false });
+    env.addFilter("date", dateFilter);
     // const issueTitle = core.getInput("issueTitle");
     // const issueBody = core.getInput("issueBody");
     const templateVariables = {
       ...github.context,
       date: Date.now()
-    }
+    };
     const octokit = new github.GitHub(token);
     // const context = github.context;
 
     const { attributes, body } = fm(file);
-    console.log(`front-matter arrtibutes: ${attributes}`)
+    console.log(`front-matter arrtibutes: ${attributes}`);
 
     const templated = {
       body: env.renderString(body, templateVariables),
       title: env.renderString(attributes.title, templateVariables)
-    }
-    core.info(attributes)
+    };
+    core.info(attributes);
     const newIssue = await octokit.issues.create({
       ...github.context.repo,
       ...templated,
       body: body,
-      assignees: assignees ? craftArray(assignees) : craftArray(attributes.assignees),
+      assignees: assignees
+        ? craftArray(assignees)
+        : craftArray(attributes.assignees),
       labels: craftArray(attributes.labels)
     });
   } catch (error) {
