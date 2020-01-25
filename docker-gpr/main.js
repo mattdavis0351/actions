081317674cf42cd930cfd05e15de4041d294960f
Matthew Davis <38021615+mattdavis0351@users.noreply.github.com>
2020-01-25 15:07:19 -0800

    Update main.js

--- docker-gpr/main.js
+++ docker-gpr/main.js
@@ -26,8 +26,7 @@ async function run() {
   try {
     await exec.exec(`docker push ${fullImageReference}`);
   } catch (err) {
-    core.error('https://help.github.com/en/github/managing-packages-with-github-packages/about-github-packages#managing-packages for more information');
-    core.setFailed(`Review the logs above, most likely you are using a package name associated with a different repository.  Rename your Image to fix.`);
+    core.setFailed(`Review the logs above, most likely you are using a package name associated with a different repository.  Rename your Image to fix. https://help.github.com/en/github/managing-packages-with-github-packages/about-github-packages#managing-packages for more information`);
   }
   core.setOutput("imageUrl", fullImageReference);
 }
